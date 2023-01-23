2647efdce90f47150ee82dfcda89ef17b2d51007
MansiRavindraMohan <manasimohan29@gmail.com>
2023-01-23 13:48:52 +0530

    Add: Print 1-10 numbers

---
+++ forloop.sh
@@ -0,0 +1,40 @@
+for (( i=1; i<=10; i++ ))
+do
+        echo $i
+done
+
+if
+	i=1
+then
+	echo $i
+fi
+if
+	i=2
+then
+	echo $i
+fi
+if
+        i=3
+then
+        echo $i
+fi
+if
+        i=5
+then
+        echo $i
+fi
+if
+        i=7
+then
+        echo $i
+fi
+if
+        i=9
+then
+        echo $i
+fi
+if
+	i=10
+then
+        echo $i
+fi
